4b872addc1d2bf264b0465f934578ff48d3d8277
Maxim <ScorpionEvil777@gmail.com>
2024-05-04 23:33:51 +0300

    Сделал на основном размрере блок о компании

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $("body").toggleClass("lock"); //Блокирует передвижение контента при открытом мению в режие мобильного устройства
   });
 });
-
+// Сполллер сервис
 document.querySelector(".button-service").onclick = (event) => {
   const spollerClass = document.querySelector(".service-list");
   spollerClass.classList.toggle("service-list-open");
